e6b7305b45d5232bd5bfaa43ecf0a98ebf0930f5
nandinidoradla13 <nandinidoradla13@gmail.com>
2026-08-04 12:04:10 +0530

    Added backend package.json

--- backend/db.js
+++ backend/db.js
@@ -1,22 +1,20 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "nandu@y",
-    database: "smart_hospital"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
-
     if (err) {
-        console.log("❌ Database Connection Failed");
+        console.log("Database Connection Failed");
         console.log(err);
         return;
     }
 
-    console.log("✅ MySQL Connected Successfully");
-
+    console.log("MySQL Connected Successfully");
 });
 
 module.exports = connection;
