aabee72f8ebfc234e1bfdd23cc3f3ae582ad56bd
nandinidoradla13 <nandinidoradla13@gmail.com>
2026-08-04 12:59:16 +0530

    Update mysql connection for railway

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,8 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+const connection = mysql.createConnection(
+    process.env.MYSQL_PUBLIC_URL
+);
 
 connection.connect((err) => {
     if (err) {
