08471f2586fa79102640b5f9c3f84f523e9c1d89
nandinidoradla13 <nandinidoradla13@gmail.com>
2026-08-04 13:45:04 +0530

    Fix mysql connection for render

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,12 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection(
-    process.env.MYSQL_PUBLIC_URL
-);
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+});
 
 connection.connect((err) => {
     if (err) {
